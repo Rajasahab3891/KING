22b83d752c2a3904a56efce2d9c19e63b01d8887
Rajasahab3891 <162156709+Rajasahab3891@users.noreply.github.com>
2024-06-29 02:38:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ayanokozii/King",
+    "https://github.com/rajasahab3891/King",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "King")
 GIT_TOKEN = getenv(
